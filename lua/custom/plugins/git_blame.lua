875e3c843b3f7cd8603ca929094e22a6477a18a8
SebastianLindback <sebbe.lindback@hotmail.com>
2023-09-25 10:40:08 +0200

    Update git_blame.lua

--- lua/custom/plugins/git_blame.lua
+++ lua/custom/plugins/git_blame.lua
@@ -1,10 +1,14 @@
 return {
   'f-person/git-blame.nvim',
   version = "*",
-  lazy = true,
+  lazy = false,
 
   config = function()
-    require('git-blame').setup {}
-
+    require('gitblame').setup {
+      enabled = true,
+      message_template = '<summary> • <date> • <author>',
+      date_format = '%r',
+      virtual_text_column = 80,
+    }
   end,
 }
