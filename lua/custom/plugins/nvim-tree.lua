89d0ff1a82bba6c6bcf156bd26cc579e7475f96f
SebastianLindback <sebbe.lindback@hotmail.com>
2023-09-29 10:48:08 +0200

    Update nvim-tree.lua

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -123,7 +123,7 @@ return {
         auto_open = true,
       },
       update_focused_file = {
-        enable = false,
+        enable = true,
         update_root = false,
         ignore_list = {},
       },
